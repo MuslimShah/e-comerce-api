6b3090752c78d2180d4f19b5bbe7748bef6ff4c5
Muslim shah <smuslimshah7@gmail.com>
2023-03-20 14:28:00 +0500

    addig start method

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
+/*
+| |
+| EXPRESS STARTER TEMPLATE |
+
+Hey there, fellow JS developer! If you're looking for a
+solid starting point for your next Express project,
+you've come to the right place! This starter template
+has everything you need to hit the ground running.
+Here's what you can expect:
+- Dummy routes to get you started
+- Express Router for cleaner code
+- Custom error classes for bad requests and server errs
+- Express-Async-Errors package for async/await error
+handling
+- Dotenv package for easy environment variable access
+- HTTP-Status-Codes package for standardized HTTP codes
+- Built using the MVC pattern for organization
+Happy coding!
+*/
+
+
 require('express-async-errors');
 require('dotenv');
 const express = require('express');
@@ -17,6 +38,8 @@ app.use(pageNotFound);
 app.use(errorHandler);
 
 
-
-
-app.listen(PORT, () => console.log(`CONNECTED ON PORT ${PORT}`))
+const start = async() => {
+    //write db connection here
+    app.listen(PORT, () => console.log(`CONNECTED ON PORT ${PORT}`))
+}
+start();
