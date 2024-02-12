29d844b413dee03617d0a1cd57a8176425c86469
muslimshah <smuslimshah7@gmail.com>
2024-02-12 15:21:56 +0500

    connecting db

--- app.js
+++ app.js
@@ -1,36 +1,21 @@
-/*
-| |
-| EXPRESS STARTER TEMPLATE |
-
-Hey there, fellow JS developer! If you're looking for a
-solid starting point for your next Express project,
-you've come to the right place! This starter template
-has everything you need to hit the ground running.
-Here's what you can expect:
-- Dummy routes to get you started
-- Express Router for cleaner code
-- Custom error classes for bad requests and server errs
-- Express-Async-Errors package for async/await error
-handling
-- Dotenv package for easy environment variable access
-- HTTP-Status-Codes package for standardized HTTP codes
-- Built using the MVC pattern for organization
-Happy coding!
-*/
-
-
 require('express-async-errors');
 require('dotenv').config();
 const express = require('express');
 const adminRoutes = require('./routes/admin')
 const pageNotFound = require('./utils/page-not-found');
 const errorHandler = require('./utils/error-handler');
+const connectDb=require('./database/database');
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'views');
 
-app.use(adminRoutes);
+app.use((req,res,next)=>{
+    console.log(req.url);
+    req.next()
+   
+})
+app.use('/api/v1/',adminRoutes);
 
 //page not found middleware
 app.use(pageNotFound);
@@ -39,7 +24,14 @@ app.use(errorHandler);
 
 
 const start = async() => {
-    //write db connection here
+    try {
+        //write db connection here
+    await connectDb(process.env.MONGO_URI);
+    console.log('connected to database');
     app.listen(PORT, () => console.log(`CONNECTED ON PORT ${PORT}`))
+    } catch (error) {
+        console.log('DB connection error');
+        
+    }
 }
 start();
--- utils/error-handler.js
+++ utils/error-handler.js
@@ -4,7 +4,7 @@ const { StatusCodes } = require('http-status-codes');
 //custom error handling middleware
 
 const errorHandler = (err, req, res, next) => {
-    if (err instanceof CusomAPIError) {
+    if (err instanceof CustomAPIError) {
         return res.status(err.statusCode).json({ msg: err.message });
     }
     // console.error(err.stack)
