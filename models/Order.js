9c806acfce2092a25db64dea1b77cba1b42272d4
muslimshah <smuslimshah7@gmail.com>
2024-03-27 00:18:04 +0500

    adding order schema

---
+++ models/Order.js
@@ -0,0 +1,58 @@
+const mongoose = require("mongoose");
+//schema for single cart item coming from frontend
+const singleCartItemSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  price: { type: Number, required: true },
+  image: { type: String, required: true },
+  amount: { type: Number, required: true },
+  product: {
+    type: mongoose.Types.ObjectId,
+    ref: "Products",
+    required: true,
+  },
+});
+
+//order schema for storing order info
+const OrderSchema = new mongoose.Schema(
+  {
+    tax: {
+      type: Number,
+      required: true,
+    },
+    shippingFee: {
+      type: Number,
+      required: true,
+    },
+    subTotal: {
+      type: Number,
+      required: true,
+    },
+    total: {
+      type: Number,
+      required: true,
+    },
+    cartItems: {
+      type: [singleCartItemSchema],
+    },
+    status: {
+      type: String,
+      enum: ["pending", "failed", "canceled", "paid", "delivered"],
+      default: "pending",
+    },
+    user: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    clientSecret: {
+      type: String,
+      required: true,
+    },
+    paymentId: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Order", OrderSchema);
